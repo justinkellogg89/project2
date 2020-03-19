9963cfb74c87dbc6abe141494a826083c8f8501b
Justin <jlkellogg18@outlook.com>
2020-03-19 12:55:03 -0700

    setup handlebars and some css

---
+++ seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO quotes (author, body, createdAt, updatedAt, UserId) VALUES ("Kim", "Honey, would you put a bumper sticker on a Bentley?", 03/19/2020, 03/19/2020, 1),
+
+INSERT INTO quotes (author, body, createdAt, updatedAt, UserId) VALUES ("Kim", "I'm kind of shocked I'm getting a fashion award when I'm naked most of the time.", 03/19/2020, 03/19/2020, 1),
+("Kim", "I used to be so dependent on the guys I was in a relationship with. I don't know why, because I wasn't raised that way. If I looked at myself at 19, I would shake myself and be like, 'Wake up. You are way too smart for this.", 03/19/2020, 03/19/2020, 1),
+("Kim", "Daddy is a singer, performer, artiste. Mommy has so many talents I can't even begin to name them.", 03/19/2020, 03/19/2020, 1),
+("Kim", "Kowabunga dudettes. I'm so pumped to be on this surfing kick. Who else surfs out there? Gnarly day in the h2o. Ridin waves!", 03/19/2020, 03/19/2020, 1),
+("Kim", "There's a lot of baggage that comes with us, but it's like Louis Vuitton baggage, you always want it.", 03/19/2020, 03/19/2020, 1),
+("Kim", "Nicole Richie reminds me of my jeep.", 03/19/2020, 03/19/2020, 1),
+("Kim", "For me, skinny is just a style of jeans, not a goal.", 03/19/2020, 03/19/2020, 1),
+
+("Khloe", "I just want to get out of here and go to my bed.", 03/19/2020, 03/19/2020, 1),
+("Khloe", "Don't know who you are, don't care.", 03/19/2020, 03/19/2020, 1),
+("Khloe", "Sometimes, you have no idea what I think in my head that I wanna do to you, and if I did them, I would go to jail for so many years.", 03/19/2020, 03/19/2020, 1),
+("Khloe", "I want you to take an STD test cause I am sure you've got a few of those!", 03/19/2020, 03/19/2020, 1),
+("Khloe", "If you start cursing at me, I will start cursing back at you. So, let's not play the curse game. 'Cause, do you want to play that game? 'Cause guess who's better at it.", 03/19/2020, 03/19/2020, 1),
+
+("Kourtney", "Honey, I could care less.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "I'll sit here and cry, watching Titanic wishing I had a little romance in my life.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "You're acting like drunk slob-kabobs.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "I start laughing at Kim when she's crying because I can't help it. She has this ugly crying face that she makes.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "Everyone is literally being so rude right now.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "Every time I get older, I look better and get smarter.", 03/19/2020, 03/19/2020, 1),
+("Kourtney", "Okay, your conversations are too long bye.", 03/19/2020, 03/19/2020, 1);
