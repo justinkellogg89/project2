060841a22edf2b3a3e4d4cbc8bbee1c5059f4b63
Megan Coppins <meganlcoppins@gmail.com>
2020-03-19 14:33:47 -0700

    async function

--- models/user.js
+++ models/user.js
@@ -19,13 +19,14 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+  User.prototype.validPassword = async function(password) {
+    const customPass = await bcrypt.compareSync(password, this.password);
+    return customPass;
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  User.addHook("beforeCreate", async function(user) {
+    user.password = await bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
   return User;
 };
