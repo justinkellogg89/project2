6c02a84817b3635e5f00d18605b38f874ed11f1e
Tucker <tuckerbeauchamp@gmail.com>
2020-03-20 12:26:15 -0700

    refactor html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,54 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
+const db = require("../models");
 
 module.exports = function(app) {
-
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-     return res.render("addSearchForms");
-    }
-    res.render("index");
+    if (!req.user) return res.render("index");
+
+    db.Quote.findAll().then(function(dbQuotes) {
+      res.render("main", {
+        layout: "loggedin",
+        quotes: dbQuotes.map(quote => quote.toJSON())
+      });
+    });
+  });
+
+  app.get("/quote/:id", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (!req.user) return res.redirect("/");
+
+    db.Quote.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: db.Comment,
+          include: [db.User]
+        }
+      ]
+    }).then(function(dbQuote) {
+
+      res.render("quote", {
+        layout: "loggedin",
+        quote: {
+          id: dbQuote.id,
+          author: dbQuote.author,
+          body: dbQuote.body
+        },
+        comments: dbQuote.Comments.map(comment => comment.toJSON())
+      });
+    });
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-     return res.render("addSearchForms");
+      return res.redirect("/");
     }
+
     res.render("login");
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.render("addSearchForms", {layout: "loggedin"});
-  });
-
 };
